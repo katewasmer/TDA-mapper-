8a0debbc206f543e407ff2b64d0845af3934bfc3
katewasmer <61073755+katewasmer@users.noreply.github.com>
2020-07-07 14:49:49 -0500

    Update PC2line.R

--- PC2line.R
+++ PC2line.R
@@ -1,8 +1,13 @@
-#2nd PCA line 
-
 plot(flares, asp=1)
 pca <- prcomp(flares) 
 
+#PCA1 line 
+x <- c(0, pca$rotation[1,1])
+y <- c(0, pca$rotation[2,1])
+mod <- lm(y ~ x)
+abline(mod, lw = 2, col = "blue")
+
+#PCA2 line 
 w <- c(0, pca$rotation[1,2])
 z <- c(0, pca$rotation[2,2])
 mod1 <- lm(z ~ w)
